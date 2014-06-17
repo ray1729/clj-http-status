5e4ee49aaad149f65a1a3a14c93d36a345388954
Ray Miller <ray@metail.com>
2014-06-17 13:34:42 +0100

    Add URL and description.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clj-http-status "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "HTTP status codes"
+  :url "https://github.com/ray1729/clj-http-status"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]])
